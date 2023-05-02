1b51102187bfedbab3cc5a71e596b270f1d2f1bf
Daniel Falcon <danfall963@gmail.com>
2023-05-02 18:54:07 -0400

    Adding Structure and Functionality

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,13 +1,18 @@
 import TodoItem from '@/components/TodoItem';
 
-const TodosList = ({ todosProps }) => {
-    return (
-      <ul>
-        {todosProps.map((todo) => (
-  <TodoItem key={todo.id} itemProp={todo} />
-))}
-      </ul>
-    );
-  };
-  export default TodosList;
+const TodosList = ({ todosProps, handleChange, delTodo }) => {
+  return (
+    <ul>
+      {todosProps.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          itemProp={todo}
+          handleChange={handleChange}
+          delTodo={delTodo}
+        />
+      ))}
+    </ul>
+  );
+};
+export default TodosList;
   
