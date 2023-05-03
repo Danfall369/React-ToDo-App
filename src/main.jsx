73ba7a2a416cd9ad29bad56ec892d1d16a5c12e5
Daniel Falcon <danfall963@gmail.com>
2023-05-02 22:40:57 -0400

    Adding Styling, scss files and buttons functions

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import '@/styles/app.scss';
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import TodoApp from '@/components/TodoApp';
 
 const domContainer = document.getElementById("root");
