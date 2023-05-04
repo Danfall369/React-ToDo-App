9d60b248537504640070614cdf6421a5e557cd3b
Daniel Falcon <danfall963@gmail.com>
2023-05-04 16:12:42 -0400

    Adding React Routers

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import '@/styles/app.scss';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoApp from '@/components/TodoApp';
+import { BrowserRouter as Router } from "react-router-dom"
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
+    <Router>
       <TodoApp />
-    </React.StrictMode>
-  );
+    </Router>
+  </React.StrictMode>
+);
